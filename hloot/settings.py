5eaf7966fab12b1fc2925b24196ca22fe5d18f1c
Zac Connelly <zconnelly13@gmail.com>
2024-06-02 19:55:12 +0200

    add settings to testing

--- hloot/settings.py
+++ hloot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+TESTING = sys.argv[1:2] == ['test']

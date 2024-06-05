821f37c24010eb5a7e4b2c9df5abbb8a8f4fd021
Zac Connelly <zconnelly13@gmail.com>
2024-06-05 05:15:20 +0200

    Revert "switch to hash router"
    This reverts commit 543c33a13ca5cdfdb0d25d4aa58b2ec00471cda0.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Host from './Host';
 import Play from './Play';
 

d9d94ac9d92387f9a2d2138a0c2fe3b9fa44530b
Zac Connelly <zconnelly13@gmail.com>
2024-06-07 09:30:53 +0200

    pass secret key env var explicitly

--- hloot/settings.py
+++ hloot/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-if not SECRET_KEY:
+if SECRET_KEY is None:
     raise ValueError("DJANGO_SECRET_KEY environment variable must be set")
 
 # SECURITY WARNING: don't run with debug turned on in production!
